fd418b16cd35adc20d4fd64bb353d76c749bd66f
kkonrad <konrad.e.urban@gmail.com>
2023-10-29 16:33:50 +0100

    boilerplate

---
+++ freelog/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DailyQuestion from './components/DailyQuestions';
+
+function App() {
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <DailyQuestion />
+        </div>
+    );
+}
+
+export default App;
